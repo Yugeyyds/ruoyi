75bd0f8ebb8ede1211e734b2e6eb0994ff3d4c7e
Yugeyyds <925582364@qq.com>
2023-03-25 14:49:53 +0800

    新增medical模块

---
+++ ruoyi-medical/src/main/java/com/ruoyi/ruoyimedical/RuoyiMedicalApplication.java
@@ -0,0 +1,13 @@
+package com.ruoyi.ruoyimedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuoyiMedicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuoyiMedicalApplication.class, args);
+    }
+
+}
---
+++ ruoyi-medical/src/test/java/com/ruoyi/ruoyimedical/RuoyiMedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruoyi.ruoyimedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuoyiMedicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
